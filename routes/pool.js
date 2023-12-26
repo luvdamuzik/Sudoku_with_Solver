2b2661e5eaafd6449e4c57c82a6262167b072141
Rijad Mutapcic <134503577+luvdamuzik@users.noreply.github.com>
2023-12-26 10:46:01 +0100

    Update pool.js

--- routes/pool.js
+++ routes/pool.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg')
 const pool = new Pool({
-    user: 'gleeuedh',
-    host: 'tyke.db.elephantsql.com',
-    database: 'gleeuedh',
-    password: '7ivoRh1NVV1LJmOmP9B4qhc4bNi7Vb4p',
+    user: '',
+    host: '',
+    database: '',
+    password: '',
     port: 5432,
     max:30,
     idleTimeoutMillis: 1000
